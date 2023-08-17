4b781236e09a8a901c93941c097055edd624ce35
Varun Poojary <varunnpoojary@gmail.com>
2023-08-17 12:29:47 +0530

    First Commit

---
+++ lib/line_chart_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smarttoiletsystem/dashboard_pages/dashboard_page_1.dart';
+
+class DashboardPage2 extends StatefulWidget {
+  final List<Color> gradientColor =[const Color(0xff575A5F),const Color(0xffB0FF4D)];
+
+
+  @override
+  State<DashboardPage2> createState() => DashboardPage2State();
+}
+
+class DashboardPage2State extends State<DashboardPage2> {
+
+  @override
+  Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
+    return
+  }
+}
+
+
